41fe0f49fa253abab433ed659e20cdd22e6f1a63
GUERRIC KERNYUY NSOSAH <guerrickernyuy@gmail.com>
2024-10-26 17:38:10 +0000

    Create function to request for patient data and add validation

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import re
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -12,9 +13,33 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('health_survey')
 
-patients = SHEET.worksheet('patients')
+min_temp = 35.0
+max_temp = 40.0
 
-data = patients.get_all_values()
+def get_patients_data():
+    """
+    Get patient name input from the user with validation.
+    Ensures the name only contains alphabetic characters and each part begins with a capital letter.
+    """
 
-print(data)
+while True:
+        print("Please enter patient's Firstname and Surname.")
+        print("Example: John Crawford \n")
+        name_str = input("Enter patient's name here: ").strip()
+        
+        if re.fullmatch(r"^[A-Z][a-z]+(?: [A-Z][a-z]+)+$", name_str):
+        
+        # r"^[A-Z][a-z]+(?: [A-Z][a-z]+)+$: expression to validate name format
+        # - ^ and $: Anchors to ensure the pattern matches the entire string.
+        # - [A-Z][a-z]+: Matches names that start with a capital letter followed by lowercase letters.
+        # - (?: [A-Z][a-z]+)+$: Ensures the second name follows the same pattern.
+            
+            print(f"Patient's name: {name_str}")
+            break
+        else:
+            print("Invalid input! Ensure each name starts with a capital letter, contains only letters, and has no special characters.")
+            print("Please try again.\n")
+
+get_patients_data()
+
 
