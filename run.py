5a05c2ba43ac2ce05c5bd4815934700ca901eb37
GUERRIC KERNYUY NSOSAH <guerrickernyuy@gmail.com>
2024-11-02 21:34:31 +0000

    Removed add redundancies and restructured code to align with syntax and indentation

--- run.py
+++ run.py
@@ -15,15 +15,21 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('health_survey')
 PATIENTS_WORKSHEET = SHEET.worksheet('patients')
 
-min_temp = 26.0 # minimum normal temperature in °C
-max_temp = 38.0 # maximum normal temperature in °C
-min_height = 61.0 # minimum height in cm
-max_height = 260.0 # maximum height in cm
-min_weight = 5.5 # minimum weight in kg
-max_weight = 700 # maximum weight in kg
-min_age = 15 # minimum age of participants in the health survey is 15 years
-max_age = 120 # maximum age of participants in the health survey is 120 years
-max_medical_condition_length = 200  # maximum character length for medical condition
+min_temp = 33.2  # minimum normal temperature in °C
+max_temp = 38.2  # maximum normal temperature in °C
+min_height = 54.6  # minimum height in cm
+max_height = 272.0  # maximum height in cm
+min_weight = 5.5  # minimum weight in kg
+max_weight = 635  # maximum weight in kg
+min_age = 15  # minimum age of participants in the health survey
+max_age = 100  # maximum age of participants
+# maximum character length for medical condition
+max_medical_condition_length = 200
+
+# ANSI escape codes for red text
+RED = "\033[91m"
+RESET = "\033[0m"  # Reset to default color
+
 
 def display_date_time():
     """
@@ -31,38 +37,49 @@ def display_date_time():
     """
     now = datetime.now()
     formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
-    print("WELCOME TO HEALTH SURVEY AUTOMATED SYSTEM. \n")
+    print("WELCOME TO HEALTH SURVEY AUTOMATED SYSTEMS \n")
     print(f"==== Version 1.0: {formatted_time} ==== \n")
 
-display_date_time()  
+
+display_date_time()
+
 
 def get_patients_data():
     """
     Get patient name input from the user with validation.
-    Ensures the name only contains alphabetic characters and each part begins with a capital letter.
+    Ensures the name only contains alphabetic characters and each part begins
+    with a capital letter.
     """
-
     while True:
         print("Please enter patient's Firstname and Surname.")
+        print("If the patient is not in the system, you will be ")
+        print("prompted to add the patient. \n")
         print("Example: John Crawford \n")
-        name_str = input("Enter patient's name here: \n").strip()
-        
-        # r"^[A-Z][a-z]+(?: [A-Z][a-z]+)+$ Regular expression to validate the name format
 
+        name_str = input("Enter patient's name here: \n").strip()
+
+        # Regular expression to validate the name format
         if re.fullmatch(r"^[A-Z][a-z]+(?: [A-Z][a-z]+)+$", name_str):
             print(f"Patient's name: {name_str}")
             print(f"Searching system for: {name_str}...")
-            search_patient_in_sheet(name_str) # Call function to search patient after validation
-            break  
+
+            # Call function to search patient after validation
+            search_patient_in_sheet(name_str)
+            break
+
         else:
-            print("Invalid input! Ensure each name starts with a capital letter, contains only letters, and has no special characters.")
+            print("Invalid input! Ensure each name starts with a ")
+            print("capital letter, contains only letters, and has no special")
+            print("characters. \n")
             print("Please try again.\n")
             print("----------------------------------------------\n")
-          
+
+
 def search_patient_in_sheet(name_str):
     """
     Searches for a patient by name in the 'patients' worksheet.
-    Prints the row with patient details if found; otherwise, notifies the user if no match is found.
+    Prints the row with patient details if found; otherwise, notifies the user
+    if no match is found.
     """
     all_patients = PATIENTS_WORKSHEET.get_all_records()
 
@@ -70,8 +87,9 @@ def search_patient_in_sheet(name_str):
     found = False
 
     # Iterate over each record to find a match by name
-    for index, patient in enumerate(all_patients, start=2):  # Start at 2 to account for header row
-        if patient['name'] == name_str:  
+    # Start at 2 to account for header row
+    for index, patient in enumerate(all_patients, start=2):
+        if patient['name'] == name_str:
             # Print the patient details
             print("\nPatient details found:")
             print(f"Name: {patient['name']}")
@@ -79,97 +97,140 @@ def search_patient_in_sheet(name_str):
             print(f"Temperature: {patient['temperature(°C)']}°C")
             print(f"Weight: {patient['weight (Kg)']} Kg")
             print(f"Height: {patient['height(cm)']} cm")
-            print(f"Existing Medical Condition: {patient['existing medical condition']}")
+            print(
+                f"Existing Medical Condition: "
+                f"{patient['existing medical condition']}"
+            )
             print(f"BMI: {patient['bmi']}\n")
             print("----------------------------------------------\n")
             found = True
-            
+
             # Ask if user wants to update details
-            update = input(f"Do you wish to update the {name_str}t's details? (yes/no): ").strip().lower()
+            update = input(
+                f"Do you wish to update {name_str}'s details? (yes/no): "
+            ).strip().lower()
+
             if update == 'yes':
-                add_new_patient(name_str, index)  # Pass row index to update the same row
+                # Pass row index to update the same row
+                add_new_patient(name_str, index)
             else:
                 print("No updates made to patient details.\n")
                 print("----------------------------------------------\n")
             break
-    
+
     if not found:
         # If no match is found, notify the user
-        print("\nNo records found with this name! Check and ensure that the name is correct.\n")
+        print("\nNo records found with this name! ")
+        print("Check and ensure that the name is correct.\n")
         print("----------------------------------------------\n")
-        reply = input(f"Would you like to Enter the name again or Add {name_str} as a new patient? (enter/add): ").strip().lower()
-        
-        if reply == 'enter':
+
+        reply = input(
+            f"Would you like to search again or add {name_str} as patient? "
+            f"(search/add): "
+        ).strip().lower()
+
+        if reply == 'search':
             get_patients_data()
-            
+
         elif reply == 'add':
             add_new_patient(name_str)
         else:
-            print("Invalid input. Please enter 'enter' to search again or 'add' to add a new patient.\n")
+            print(
+                "Invalid input! "
+                "Please enter 'search' to try again or 'add' "
+            )
+            print("to add a new patient.\n")
             print("----------------------------------------------\n")
+
 
 def add_new_patient(name_str, row=None):
     """
-    Prompts the user to confirm if patient is new and, if yes, to enter patient details.
-    Validates each input before adding to the sheet.
-    """  
-    response = input(f"Are you confirming to add {name_str}'s details in the system? (yes/no): ").strip().lower()
+    Prompts the user to confirm if patient is new and, if yes, to enter
+    patient details and validates each input before adding to the sheet.
+    """
+    response = input(
+        f"Are you confirming to add {name_str}'s details in the system?"
+        f"(yes/no): "
+    ).strip().lower()
+
     if response == 'yes':
         print(f"Please enter the following details for: {name_str}:\n")
         print("----------------------------------------------\n")
 
-        #Validates each entry befor storing it 
+        # Validates each entry before storing it
         while True:
             try:
-
-                #Age validation: Age must be an Integer and within range
-                # Age validation with retry loop
+                # Age validation: Age must be an Integer and within range
                 while True:
                     age = int(input("Enter age: \n").strip())
-                    if age < min_age:
-                        raise ValueError(f"Age must be at least {min_age} years.")
+                    if not (min_age <= age <= max_age):
+                        raise ValueError(
+                            f"Age must be "
+                            f">= {min_age} and <= {max_age}years."
+                        )
                     break  # Break loop if age is valid
 
-                # Temperature validation (it must be a float in the range of min_temp and max_temp defined above)
                 # Temperature validation
                 while True:
-                    temperature = float(input("Enter temperature (°C): \n").strip())
+                    temperature = float(input(
+                        "Enter temperature (°C): \n"
+                    ).strip())
                     if not (min_temp <= temperature <= max_temp):
-                        print(f"Caution!!! Temperature is out of range ({min_temp} - {max_temp}°C).")
-                    break  # Proceed regardless, as temperature can be recorded even if out of range
+                        print(
+                            f"{RED}Caution!!! Temperature is out of range "
+                            f"({min_temp} - {max_temp}°C).{RESET}"
+                        )
+                    break  # Temperature can be recorded even if out of range
 
-                # Weight validation: must be a float within min and max weight range
                 # Weight validation with retry loop
                 while True:
                     weight = float(input("Enter weight (Kg): \n").strip())
                     if not (min_weight <= weight <= max_weight):
-                        raise ValueError(f"Weight must be between {min_weight} and {max_weight} Kg.")
+                        raise ValueError(
+                            f"Weight must be between "
+                            f"{min_weight} and {max_weight} Kg."
+                        )
                     break  # Break loop if weight is valid
 
-                # Height validation: must be an integer within min and max height range
-                # Height validation with retry loop
+                # Height validation
                 while True:
                     height = int(input("Enter height (cm): \n").strip())
                     if not (min_height <= height <= max_height):
-                        raise ValueError(f"Height must be between {min_height} and {max_height} cm.")
+                        raise ValueError(
+                            f"Height must be between {min_height} and "
+                            f"{max_height} cm."
+                        )
                     break  # Break loop if height is valid
 
                 # Calculate BMI
                 bmi = round(weight / (height / 100) ** 2, 2)
                 print(f"{name_str}'s BMI is: {bmi}\n")
 
-                # Medical condition (must be a string, no special validation applied here)
                 # Medical condition validation
                 while True:
-                    medical_condition = input(f"Enter {name_str}'s existing medical condition (max {max_medical_condition_length} characters): ").strip()
+                    medical_condition = input(
+                        f"Enter {name_str}'s existing medical condition "
+                        f"(max {max_medical_condition_length} characters): "
+                    ).strip()
+
                     if len(medical_condition) > max_medical_condition_length:
-                        raise ValueError(f"Medical condition should be less than {max_medical_condition_length} characters.")
+                        raise ValueError(
+                            f"Medical condition should be less than "
+                            f"{max_medical_condition_length} characters."
+                        )
                     break  # Break loop if medical condition is valid
-                
-                confirm = input(f"Do you confirm to add {name_str} to the system? (yes/no): \n").strip().lower()
+
+                confirm = input(
+                    f"Do you confirm to add {name_str} to the system? "
+                    "(yes/no): \n"
+                ).strip().lower()
+
                 if confirm == 'yes':
                     # Append new patient details to the worksheet
-                    PATIENTS_WORKSHEET.append_row([name_str, age, temperature, weight, height, medical_condition, bmi])
+                    PATIENTS_WORKSHEET.append_row(
+                        [name_str, age, temperature, weight, height,
+                         medical_condition, bmi]
+                    )
                     print("System updating...\n")
                     print(f"\nPatient {name_str} added successfully!\n")
                     print("----------------------------------------------\n")
